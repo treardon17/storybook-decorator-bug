36f07f06b7cfc8a6aedae8d8f850330990e2de58
Tyler Reardon <tdr.reardon@gmail.com>
2023-02-23 15:41:54 -0800

    setup project

---
+++ src/stories/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-btn @click="onClick" :color="color">{{ label }}</v-btn>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, onBeforeUnmount } from 'vue';
+
+withDefaults(defineProps<{
+  /**
+   * The label of the button
+   */
+  label: string,
+  /**
+   * background color of the button
+   */
+  color?: string,
+}>(), { color: 'primary' });
+
+const emit = defineEmits<{
+  (e: 'click', id: Event): void;
+}>();
+
+const onClick = (e: Event) => {
+  emit('click', e)
+};
+
+onMounted(() => {
+  console.log('mounted!');
+});
+
+onBeforeUnmount(() => {
+  console.log('unmount - this should not show when args change');
+});
+</script>
